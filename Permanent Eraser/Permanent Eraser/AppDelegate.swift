34ac689ec44685e14dec13ab3be5538861832520
Chad Armstrong <chad@edenwaith.com>
2020-11-16 15:49:07 -0700

    [3.0.0] New version for PE 3.0
    - New verion for PE 3.0, which replaces the previous version

---
+++ Permanent Eraser/Permanent Eraser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Permanent Eraser
+//
+//  Created by Administrator on 11/16/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Permanent Eraser/Permanent Eraser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Permanent Eraser
+//
+//  Created by Administrator on 11/16/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
