fdf06873d170252417f5fd20c83bd472712172a2
Chad Armstrong <chad@edenwaith.com>
2021-07-05 21:09:37 -0600

    [3.0.0] Status Bar set up + Icons
    - Initial set up for the status bar
    - Added some icons and working with SF Symbols
    - Updating Info.plist and app settings

--- Permanent Eraser/Permanent Eraser/AppDelegate.swift
+++ Permanent Eraser/Permanent Eraser/AppDelegate.swift
@@ -11,16 +11,40 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
-
+	@IBOutlet weak var statusBarMenu: NSMenu!
+	var statusBarItem: NSStatusItem!
+	
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+		setupStatusBarMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+	// MARK: -
+	
+	func setupStatusBarMenu() {
+		
+		statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+		// statusBarItem.button?.image = NSImage(named: NSImage.trashFullName)
+		if #available(macOS 11.0, *) {
+			if let trashImage = NSImage(systemSymbolName: "trash", accessibilityDescription: "Permanent Eraser") {
+				trashImage.isTemplate = false
+				statusBarItem.button?.image = trashImage
+			}
+		} else {
+			// Fallback on earlier versions
+			statusBarItem.button?.image = NSImage(named: NSImage.trashEmptyName)
+		}
+		
+		statusBarItem.menu = self.statusBarMenu
+	}
 
+	@IBAction func quitApplication(_ sender: Any) {
+		NSApp.terminate(sender)
+	}
 }
 
